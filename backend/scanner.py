f0fe3865eea4862f94863c46ca016d9930d8351a
danila seliayeu <dan.seliayeu@gmail.com>
2023-09-16 23:56:06 -0400

    update corp

--- backend/scanner.py
+++ backend/scanner.py
@@ -57,13 +57,13 @@ def extract_cost(flyer_text: str) -> float:
     flyer_text = flyer_text.replace("\\\n", " ")
     flyer_text = flyer_text.replace("\n", " ")
     flyer_words = [ normalize("NFKC", w) for w in flyer_text.split(" ") ]
-    print( flyer_words)
-    costs = [ float(normalize("NFKC", w)) for w in flyer_words if (len(w) >= 3 and w.isdigit() or w in ["4.99", "14.99", "4.50", "14.50"] and ((w[-1] == '9') or w[-2:] == "50")) ]
+    costs = [ w for w in flyer_words if (len(w) >= 3 and (w.isdigit() or w in ["4.99", "14.99", "4.50", "14.50"]))]
+    costs = [ float(w) for w in costs if w[-1] == '9' or w[-2:] == '50']
     print(costs)
     return costs[0] / 100 if costs[0] > 100 else costs[0]
 
 if __name__ == "__main__":
-    for i in range(10):
+    for i in range(12):
         if i == 3: continue
         flyer_text = extract_flyer(f"https://raw.githubusercontent.com/recipede/recipe-detect/main/grocery/crop_{i}.jpg")
         print(extract_cost(flyer_text))
