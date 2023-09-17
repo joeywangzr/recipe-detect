1149e6c4c9ba116192969cd3886d465a94a66b68
danila seliayeu <dan.seliayeu@gmail.com>
2023-09-16 23:42:37 -0400

    add more files for testing

--- backend/scanner.py
+++ backend/scanner.py
@@ -4,6 +4,8 @@ from google.cloud import vision
 import cohere  
 import os
 from dotenv import load_dotenv
+from unicodedata import normalize
+
 
 load_dotenv()
 
@@ -54,10 +56,14 @@ def extract_flyer(image_uri: str) -> str:
 def extract_cost(flyer_text: str) -> float:
     flyer_text = flyer_text.replace("\\\n", " ")
     flyer_text = flyer_text.replace("\n", " ")
-    flyer_words = flyer_text.split(" ")
-    costs = [ float(w) for w in flyer_words if (len(w) >= 3 and (w[-1] == '9') or w[-2:] == "50") ]
-    return costs[0] / 100
+    flyer_words = [ normalize("NFKC", w) for w in flyer_text.split(" ") ]
+    print( flyer_words)
+    costs = [ float(normalize("NFKC", w)) for w in flyer_words if (len(w) >= 3 and w.isdigit() or w in ["4.99", "14.99", "4.50", "14.50"] and ((w[-1] == '9') or w[-2:] == "50")) ]
+    print(costs)
+    return costs[0] / 100 if costs[0] > 100 else costs[0]
 
 if __name__ == "__main__":
-    flyer_text = extract_flyer("https://raw.githubusercontent.com/recipede/recipe-detect/main/backend/flyers/flyer3.png")
-    print(extract_cost(flyer_text))
+    for i in range(10):
+        if i == 3: continue
+        flyer_text = extract_flyer(f"https://raw.githubusercontent.com/recipede/recipe-detect/main/grocery/crop_{i}.jpg")
+        print(extract_cost(flyer_text))
